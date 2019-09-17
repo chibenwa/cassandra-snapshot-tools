096bebf5eb3f2d60ef91e72b3afe45dbcf4ab054
Benoit Tellier <btellier@linagora.com>
2019-09-17 13:01:07 +0700

    Script for restauring a snapshot assuming empty existing tables

---
+++ localDataRestore.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Usage ./localDataRestore.sh $1
+# Where $1 is the keyspace where to restore all data
+# Note: cqlsh needs to be installed
+
+printUsage() {
+   echo "Usage : "
+   echo "./localDataRestore.sh SNAPSHOT_TAR KEYSPACE"
+   echo "    KEYSPACE: the keyspace where to restore all data"
+   echo "    SNAPSHOT_TAR: the snapshot tarball to restore data from"
+   echo ""
+   echo "Note: [nodetool cp tar rm mkdir grep cut] needs to be installed"
+   exit 1
+}
+
+# Validate arguments
+if [ -z "$1" ]; then
+    printUsage
+fi
+if [ -z "$2" ]; then
+    printUsage
+fi
+if ! [ -z "$3" ]; then
+    printUsage
+fi
+SNAPPKG=$1
+KEYSPACE=$2
+echo "Restoring data stored in ${SNAPPKG} into ${KEYSPACE}"
+
+# Checking nodetool cp tar presence
+./check_dependencies.sh nodetool cp tar rm mkdir grep cut
+RETVAL=$?
+if [[ "$RETVAL" != "0" ]]; then
+  exit 1
+fi
+
+TEMPDIR="$( pwd )/.localDataRestore.tmp${RANDOM}"
+# Remove local temp directory
+[ "$TEMPDIR" != "/" ] && rm -rf "$TEMPDIR"
+
+# Verify/Extract Snapshot Package
+tar -tvf "$SNAPPKG" 2>&1 | grep "$KEYSPFILE" 2>&1 >/dev/null
+RC=$?
+if [ $RC -gt 0 ]; then
+        echo "Snapshot package $SNAPPKG appears invalid or corrupt"
+        exit 1
+fi
+
+# Create temporary working directory
+if [ ! -d "$TEMPDIR" ] && [ ! -e "$TEMPDIR" ]; then
+    echo "Creating ${TEMPDIR}"
+    mkdir -p "$TEMPDIR"
+else
+    echo "Error creating temporary directory $TEMPDIR"
+    exit 1
+fi
+
+# Extract snapshot package
+tar -xf "$SNAPPKG" --directory "$TEMPDIR"
+chown -R cassandra:cassandra $TEMPDIR
+
+for columnfamily in `ls "${TEMPDIR}/${KEYSPACE}"`; do
+    TABLE=`echo "${columnfamily}" | cut -d '-' -f 1`
+    echo "Copying data into ${TABLE} data directory"
+    cp -p $TEMPDIR/$KEYSPACE/$columnfamily/* /var/lib/cassandra/data/$KEYSPACE/$columnfamily/.
+    echo "Refreshing ${TABLE}"
+    nodetool refresh ${KEYSPACE} ${TABLE}
+done
+
+# Cleanup tmp directory
+rm -rf "$TEMPDIR"
+
+echo "Data successfully restored from ${SNAPPKG} into ${KEYSPACE}"
